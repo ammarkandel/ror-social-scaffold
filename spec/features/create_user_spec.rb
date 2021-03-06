b2414a5334ace53f73fbee876c738ca244f9928d
ammarkandel <merom8703@gmail.com>
2021-03-06 17:57:02 +0200

    Fix linter alerts

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe 'User create', type: :feature do
   scenario 'User creation' do
     visit new_user_registration_path
-    fill_in 'user_name', with: 'mero'
-    fill_in 'user_email', with: 'mero@gmail.com'
-    fill_in 'user_password', with: 'password'
-    fill_in 'user_password_confirmation', with: 'password'
+    fill_in 'user_name',  with: 'mero'
+    fill_in 'user_email',  with: 'mero@gmail.com'
+    fill_in 'user_password',  with: 'password'
+    fill_in 'user_password_confirmation',  with: 'password'
     click_on 'Sign up'
     expect(page).to have_content('Welcome!')
   end
