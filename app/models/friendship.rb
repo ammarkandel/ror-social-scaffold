2ce639c987de5a1b3dd6797403703da41500246c
ammarkandel <merom8703@gmail.com>
2021-03-08 01:01:04 +0200

    Fix Linter alerts

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,11 +3,9 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   def confirm_friend
-    update_all(confirmed: true)
+    recent.update_all(confirmed: true)
     Friendship.create! do |f|
-      f.friend_id = user_id,
-      f.user_id = friend_id,
-      f.confirmed = true
+      f.friend_id = user_id, f.user_id = friend_id, f.confirmed = true
     end
   end
 end
