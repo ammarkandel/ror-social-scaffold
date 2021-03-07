06ccf2f7e15edbd5a0c9b7cb275e26df97820cb8
ammarkandel <merom8703@gmail.com>
2021-03-07 21:58:45 +0200

    Fix Linter Alerts

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,7 +3,7 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   def confirm_friend
-    self.update_attributes(confirmed: true)
+    update_attributes(confirmed: true)
     Friendship.create!(
       friend_id: user_id,
       user_id: friend_id,
