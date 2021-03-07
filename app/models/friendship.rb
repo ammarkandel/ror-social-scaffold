a70861fa890e6208203f66eb47b83f8b7483e36e
ammarkandel <merom8703@gmail.com>
2021-03-08 00:58:13 +0200

    Modify confirm_friend method

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,11 +3,11 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   def confirm_friend
-    update_attributes(confirmed: true)
-    Friendship.create!(
-      friend_id: user_id,
-      user_id: friend_id,
-      confirmed: true
-    )
+    update_all(confirmed: true)
+    Friendship.create! do |f|
+      f.friend_id = user_id,
+      f.user_id = friend_id,
+      f.confirmed = true
+    end
   end
 end
